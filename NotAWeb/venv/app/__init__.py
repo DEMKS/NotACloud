5aad8d3e1d85a9a84db8d0b805b151275b83b776
DEMKS <bot.335673@gmail.com>
2020-05-31 11:25:56 +0300

    Unstable dev

--- NotAWeb/venv/app/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-app=Flask(__name__, static_folder='../../../')
-app.config.from_object('config')
-
-from app import views
--- NotAWeb/venv/app/forms.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import Form
-import wtforms
-from wtforms import TextField, BooleanField, PasswordField
-from wtforms.validators import Required
-class LoginForm(Form):
-    remember_me = wtforms.BooleanField('remember_me',default=False)
